06715a2a44d23a33e3c36701d0c8ad56e58897a5
AbhishekRH <abhishek7.rh@gmail.com>
2024-09-15 22:48:29 +0530

    cors update 2

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ app.use(
     // origin: "http://localhost:5173", // allow requests from your frontend
     origin:
       process.env.NODE_ENV === "production"
-        ? "https://your-vercel-domain.vercel.app"
+        ? "https://mern-product-store-eta.vercel.app/"
         : "http://localhost:5173",
 
     methods: ["GET", "POST", "PUT", "DELETE"],
